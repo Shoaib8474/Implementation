fbd9b91e5048218bc0e702a85a4b726af63397e8
Shoaib Alom <shoaibs4444@gmail.com>
2025-04-26 12:43:37 +0530

    Rotate array

---
+++ Rotate_array.js
@@ -0,0 +1,58 @@
+//Q. JavaScript Program to left rotate the array by d positions
+
+//1. Navive approch [O(n*d) & O(1)]
+// Function to left rotate array by d positions
+function rotateArr(arr, d) {
+    let n = arr.length;
+    d %= n;
+    // Loop rotate the array to left by d positions
+    for (let i = 0; i < d; i++) {
+        //loop shift the array towards left by one
+        let first = arr[0];
+        for (let j = 0; j < n - 1; j++) {
+            arr[j] = arr[j + 1];
+        }
+        arr[n - 1] = first;
+    }
+}
+
+let arr = [1, 2, 3, 4, 5, 6];
+let d = 9; // towards left by d positions
+rotateArr(arr, d);
+console.log(arr.join(" "));
+//Expected Output
+//Output: {3, 4, 5, 6, 1, 2}
+//Explanation: After first left rotation, arr[] becomes {2, 3, 4, 5, 6, 1}
+// and after the second rotation, arr[] becomes {3, 4, 5, 6, 1, 2}
+
+
+//2. Better approch [O(n) & O(n)]
+// Function to left rotate array by k positions
+function rotateArr1(arr, k) {
+    let n = arr.length;
+    k %= n;
+
+    // Storing rotated version of array
+    let temp = new Array(n);
+
+    // Loop to copy last (n - k) elements to the front of temp
+    for (let i = 0; i < n - k; i++) {
+        temp[i] = arr[i + k];
+    }
+
+    //Loop to copy the first d elements to the back of temp
+    for (let j = 0; j < k; j++) {
+        temp[n - k + j] = arr[j];
+    }
+
+    // Copying the elements of temp in arr
+    // to get the final rotated array
+    for (let i = 0; i < n; i++) {
+        arr[i] = temp[i];
+    }
+}
+let arr1 = [1, 2, 3, 4, 5, 6];
+let k = 2;
+rotateArr1(arr1, k);
+console.log(arr1.join(" "));
+//Expected Output: { 3 4 5 6 1 2 }
